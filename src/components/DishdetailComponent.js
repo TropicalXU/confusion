e617272ea0cb743736464c25af906eba850e23ae
Sean Hackett <100976172+TropicalXU@users.noreply.github.com>
2022-09-13 21:40:24 -0400

    React animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,8 @@ Button, Modal, ModalHeader, ModalBody, Row, Label, Col  } from 'reactstrap';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl'
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 //  VALIDATORS
 const required = (val) => val && val.length;
@@ -135,15 +136,20 @@ class CommentForm extends Component {
             return (
                
                 <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width='100%' src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>
-                            {dish.description}
-                        </CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in
+                        tranformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg width='100%' src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>
+                                {dish.description}
+                            </CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>
 
             );
@@ -157,15 +163,17 @@ class CommentForm extends Component {
     function RenderComments({comments, postComment, disId}) {
         const comms = comments.map(comment => {
             return(
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- <i>{comment.author}</i>, &nbsp; 
-                    {new Intl.DateTimeFormat('en-US',
-                    {year: 'numeric',
-                     month: 'short',
-                     day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                    </p>
-                </li>
+                <Fade in>
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- <i>{comment.author}</i>, &nbsp; 
+                        {new Intl.DateTimeFormat('en-US',
+                        {year: 'numeric',
+                        month: 'short',
+                        day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    </li>
+                </Fade>
             );
         });
         if(comments != null) {
@@ -173,7 +181,9 @@ class CommentForm extends Component {
                 <div className='col-12 col-md-5 m-1'>
                    <h4>Comments</h4>
                    <ul className='list-unstyled'>
-                      {comms}
+                        <Stagger in>
+                            {comms}
+                        </Stagger>
                    </ul>
                    <CommentForm disId={disId} postComment={postComment} />
                 </div>
